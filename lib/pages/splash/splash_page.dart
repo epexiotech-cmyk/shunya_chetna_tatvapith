f23563b33fb321a0e7099b0372f3ba3a3ded8e77
Apurv Patel <95409482+apurv9112@users.noreply.github.com>
2026-03-06 02:27:53 +0530

    splash screen issue resolve

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -5,33 +5,40 @@ import 'package:shunya_app/utils/responsive.dart';
 import 'package:shunya_app/widgets/custom_text.dart';
 import 'splash_controller.dart';
 
-class SplashScreen extends GetView<SplashController> {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.WHITE,
-      body: Padding(
-        padding: EdgeInsets.only(top: hp(30), left: wp(10), bottom: hp(0.2)),
-        child: FadeTransition(
-          opacity: controller.fadeAnimation,
-          child: SlideTransition(
-            position: controller.slideAnimation,
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/images/splash_logo.png',
-                  scale: dp(context, 2.3),
-                  color: AppColors.PRIMARY_COLOR,
+    return GetBuilder<SplashController>(
+      init: SplashController(),
+      builder: (controller) {
+        return Scaffold(
+          backgroundColor: AppColors.WHITE,
+          body: Center(
+            child: FadeTransition(
+              opacity: controller.fadeAnimation,
+              child: SlideTransition(
+                position: controller.slideAnimation,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      'assets/images/splash_logo.png',
+                      scale: dp(context, 2.3),
+                      color: AppColors.PRIMARY_COLOR,
+                    ),
+
+                    SizedBox(height: hp(2)),
+
+                    CustomText(text: "Patient Management System"),
+                  ],
                 ),
-                SizedBox(height: hp(2)),
-                CustomText(text: "Patient Management System"),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
